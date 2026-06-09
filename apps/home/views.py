067be2da5800e90316deb7e8ad3c5cc34faf5545
Dinesh K <d860848@gmail.com>
2026-06-09 22:00:05 +0530

    Add dynamic projects model admin and portfolio projects section

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from apps.projects.models import Project
 
+def home(request):
+        projects = Project.objects.all()
+        return render(request, 'home/index.html', {'projects': projects})
 # Create your views here.
 def home(request):
     return render(request, 'home/index.html')
--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='project_images/')
+    github_link = models.URLField()
+    live_link = models.URLField(blank=True)
+def __str__(self):
+    return self.title
