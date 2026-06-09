84c280c1cc903c80ca04d57a1fb6a7f6e239020c
Dinesh K <d860848@gmail.com>
2026-06-09 23:05:51 +0530

    added projects section and portfolio cards

--- apps/home/views.py
+++ apps/home/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from apps.projects.models import Project
 
 def home(request):
-        projects = Project.objects.all()
-        return render(request, 'home/index.html', {'projects': projects})
-# Create your views here.
-def home(request):
-    return render(request, 'home/index.html')
+    projects = Project.objects.all()
+
+    return render(
+        request,
+        "home/index.html",
+        {"projects": projects}
+    )
--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-# Create your models here.
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='project_images/')
     github_link = models.URLField()
     live_link = models.URLField(blank=True)
-def __str__(self):
-    return self.title
+
+    def __str__(self):
+        return self.title
