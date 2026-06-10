e60743495c930319523109dc0fe2197197e7e4fc
Dinesh K <d860848@gmail.com>
2026-06-10 22:48:03 +0530

    added resume download feature

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from apps.contact.models import Contact
 from apps.projects.models import Project
+from apps.contact.models import Contact
 
 def home(request):
-
     projects = Project.objects.all()
+    success = False
 
     if request.method == "POST":
         Contact.objects.create(
@@ -13,9 +13,13 @@ def home(request):
             subject=request.POST.get("subject"),
             message=request.POST.get("message")
         )
+        success = True
 
     return render(
         request,
         "home/index.html",
-        {"projects": projects}
+        {
+            "projects": projects,
+            "success": success
+        }
     )
